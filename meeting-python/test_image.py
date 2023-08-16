2d6f2d699db9cf1061efc3e772bc6d23f24a012d
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-16 16:59:40 +0300

    определение модели и  типа файла

--- meeting-python/test_image.py
+++ meeting-python/test_image.py
@@ -1,5 +1,5 @@
 from PIL import Image
 
 image = Image.open('D:\Develop\DevMan\meeting-python\\1.png')
-rotated_image = image.rotate(45)
-rotated_image.save('D:\Develop\DevMan\meeting-python\\2.png')
+print(image.format)
+print(image.mode)
