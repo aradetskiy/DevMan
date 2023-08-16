d0d25936df4866f6ff4408b8b203ec354988077f
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-16 17:00:59 +0300

    e

--- meeting-python/test_image.py
+++ meeting-python/test_image.py
@@ -1,5 +1,5 @@
 from PIL import Image
 
-image = Image.open('D:\Develop\DevMan\meeting-python\\1.png')
+image = Image.open('D:\\Develop\\DevMan\\meeting-python\\1.png')
 print(image.format)
 print(image.mode)
