9ffba602e65bc2481d16f9c707981a93dc40a8a2
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-17 19:44:01 +0300

    Урок 5

--- meeting-python/less4.py
+++ meeting-python/less4.py
@@ -1,6 +1,17 @@
 from PIL import Image
+from os import remove
 
-image = Image.open('meeting-python\\3.jpg')
-new_image = image.rotate(45)
-new_image.show()
-#new_image.save('meeting-python\\4.jpg')
+im = Image.open('meeting-python\\monro.jpg')
+im_rgb=im.convert('RGB')
+print(f'Ширина - {im_rgb.width} \nВысота - {im_rgb.height}\nЦветовая модель - {im_rgb.mode} ')
+red,green,blue = im_rgb.split()
+red.save('meeting-python\\monro_red.jpg')
+green.save('meeting-python\\monro_green.jpg')
+blue.save('meeting-python\\monro_blue.jpg')
+remove('meeting-python\\monro.jpg')
+
+monro_new = Image.merge('RGB',(red,green,blue))
+
+
+
+monro_new.show()
