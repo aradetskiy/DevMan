382811d38aecb506e29e1957431a43a52b9a7247
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-17 17:35:19 +0300

    деление по каналам

--- meeting-python/test_image.py
+++ meeting-python/test_image.py
@@ -1,7 +1,6 @@
 from PIL import Image
 
-image = Image.open('meeting-python\\3.jpg')
-coordinates = (100,200, image.width-100, image.height-100)
-new_image = image.crop(coordinates)
+im = Image.open('meeting-python\\3.jpg')
+red,gree, blue = im.split()
+new_image = Image.merge('RGB',(red,gree, blue))
 new_image.save('meeting-python\\4.jpg')
-print(image.size)
