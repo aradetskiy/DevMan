13db3672f5ff94ac318b1d8353c27b0c91fa36cc
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-17 19:15:16 +0300

    совмещение картинок

--- meeting-python/test_image.py
+++ meeting-python/test_image.py
@@ -2,12 +2,13 @@ from PIL import Image
 
 im = Image.open('meeting-python\\3.jpg')
 
-width,high = im.size
+coord_left = (100, 0, im.width, im.height)
+im_left = im.crop(coord_left)
+coord_medium = (50,0,im.width-50,im.height)
+im_medium = im.crop(coord_medium)
+im_blend = Image.blend(im_medium,im_left,0.8)
+im_blend.show()
 
-print(im.size)
-new_size = (int(width/2), int(high/2))
-new_im = im.resize(new_size)
-#im.thumbnail((1200, 768))
-print(new_im.size)
-new_im.show()
+
+
 
