3cbeabe17f318988c50cfb72c64fa858ae609783
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-17 18:10:49 +0300

    изменение размера

--- meeting-python/less4.py
+++ meeting-python/less4.py
@@ -2,4 +2,5 @@ from PIL import Image
 
 image = Image.open('meeting-python\\3.jpg')
 new_image = image.rotate(45)
-new_image.save('meeting-python\\4.jpg')
+new_image.show()
+#new_image.save('meeting-python\\4.jpg')
--- meeting-python/test_image.py
+++ meeting-python/test_image.py
@@ -1,6 +1,13 @@
 from PIL import Image
 
 im = Image.open('meeting-python\\3.jpg')
-red,gree, blue = im.split()
-new_image = Image.merge('RGB',(red,gree, blue))
-new_image.save('meeting-python\\4.jpg')
+
+width,high = im.size
+
+print(im.size)
+new_size = (int(width/2), int(high/2))
+new_im = im.resize(new_size)
+#im.thumbnail((1200, 768))
+print(new_im.size)
+new_im.show()
+
