53f9ae86613300362349bdec0339d7f9477a16ed
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-17 20:30:18 +0300

    Разобраться с шириной с отрицательными числами

--- meeting-python/less4.py
+++ meeting-python/less4.py
@@ -2,16 +2,32 @@ from PIL import Image
 from os import remove
 
 im = Image.open('meeting-python\\monro.jpg')
-im_rgb=im.convert('RGB')
-print(f'Ширина - {im_rgb.width} \nВысота - {im_rgb.height}\nЦветовая модель - {im_rgb.mode} ')
-red,green,blue = im_rgb.split()
+im_rgb = im.convert('RGB')
+print(
+    f'Ширина  канала- {im.width} \nВысота  канала - {im.height}')
+red, green, blue = im_rgb.split()
 red.save('meeting-python\\monro_red.jpg')
 green.save('meeting-python\\monro_green.jpg')
 blue.save('meeting-python\\monro_blue.jpg')
-remove('meeting-python\\monro.jpg')
+# remove('meeting-python\\monro.jpg')
+monro_new = Image.merge('RGB', (red, green, blue))
 
-monro_new = Image.merge('RGB',(red,green,blue))
+def change_channel(im,left_crop_width,left_crop_high,right_crop_width,right_crop_high):
+    coord_channel_left = (left_crop_width, left_crop_high, right_crop_width, right_crop_high)
+    im_left = im.crop(coord_channel_left)
+    coord_channel_medium = (int(left_crop_width/2), left_crop_high, right_crop_width-int(left_crop_width/2), right_crop_high)
+    im_medium = im.crop(coord_channel_medium)
+    im_new = Image.blend(im_medium,im_left,0.7)
+    
+    return im_new
 
 
+monro_red_new = change_channel(red,80,0,red.width,red.height)
+print(
+    f'Ширина красного канала- {monro_red_new.width} \nВысота красного канала - {monro_red_new.height}')
+monro_blue_new = change_channel(blue,-80,0,red.width,red.height)
+print(
+    f'Ширина синего канала- {monro_blue_new.width} \nВысота синего канала - {monro_blue_new.height}')
 
-monro_new.show()
+monro_blue_new.show()
+
