b98e1131670963577de56dfd0442d5560874509f
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-18 13:27:14 +0300

    скорректировал наименования переменных

--- meeting-python/less4.py
+++ meeting-python/less4.py
@@ -1,45 +1,45 @@
 from PIL import Image
 from os import remove
 
-im = Image.open('meeting-python\\monro.jpg')
-im_rgb = im.convert('RGB')
-red, green, blue = im_rgb.split()
+img = Image.open('meeting-python\\monro.jpg')
+img_rgb = img.convert('RGB')
+red, green, blue = img_rgb.split()
 remove('meeting-python\\monro.jpg')
 
 
-def shadow_image(im, count_move, im_width, im_height, unvisibility_shadow):
-    if count_move > 0:
-        im_coord_right = (0, 0, im_width - int(count_move*2), im_height)
-        im_right = im.crop(im_coord_right)
-        im_coord_medium = (count_move, 0, im_width-count_move, im_height)
-        im_medium = im.crop(im_coord_medium)
-        im_new = Image.blend(im_medium, im_right, unvisibility_shadow)
+def shadow_image(img, count_shift, img_width, img_height, unvisibility_shadow):
+    if count_shift > 0:
+        img_coord_right = (0, 0, img_width - int(count_shift*2), img_height)
+        img_right = img.crop(img_coord_right)
+        img_coord_medium = (count_shift, 0, img_width-count_shift, img_height)
+        img_medium = img.crop(img_coord_medium)
+        img_new = Image.blend(img_medium, img_right, unvisibility_shadow)
     else:
-        count_move = abs(count_move)
-        im_coord_left = (int(count_move*2), 0, im_width, im_height)
-        im_left = im.crop(im_coord_left)
-        im_coord_medium = (count_move, 0, int(im_width-count_move), im_height)
-        im_medium = im.crop(im_coord_medium)
-        im_new = Image.blend(im_medium, im_left, unvisibility_shadow)
-    return im_new
+        count_shift = abs(count_shift)
+        img_coord_left = (int(count_shift*2), 0, img_width, img_height)
+        img_left = img.crop(img_coord_left)
+        img_coord_medium = (count_shift, 0, int(img_width-count_shift), img_height)
+        img_medium = img.crop(img_coord_medium)
+        img_new = Image.blend(img_medium, img_left, unvisibility_shadow)
+    return img_new
 
-# count_move_shadow -  count pixel to move shadow.
+# count_shift_shadow -  count pixel to move shadow.
 # If you want move shadow right you should choose positive integer number
 # If you want move shadow left you should choose negаtive integer number
 
 
-count_move_shadow = 10
+count_shift_shadow = 10
 # if want increase visibility shadow you should decrease unvisibility_shadow
 unvisibility_shadow = 0.7
 
 monro_red_new = shadow_image(
-    red, count_move_shadow, red.width, red.height, unvisibility_shadow)
+    red, count_shift_shadow, red.width, red.height, unvisibility_shadow)
 
 monro_blue_new = shadow_image(
-    blue, -count_move_shadow, blue.width, blue.height, unvisibility_shadow)
+    blue, -count_shift_shadow, blue.width, blue.height, unvisibility_shadow)
 
-coord_monro_green_new = (count_move_shadow, 0, int(
-    green.width-count_move_shadow), green.height)
+coord_monro_green_new = (count_shift_shadow, 0, int(
+    green.width-count_shift_shadow), green.height)
 monro_green_new = green.crop(coord_monro_green_new)
 
 
