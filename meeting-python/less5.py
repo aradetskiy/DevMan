b04f21787a0dd67fdcb3600387fc2c45e83b9e10
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-22 17:39:07 +0300

    Урок 8

--- meeting-python/less5.py
+++ meeting-python/less5.py
@@ -1,22 +1,31 @@
 from file_operations import VERSION ,render_template
 from faker import Faker
-
+from random import randint,choice
 fake = Faker('ru_RU')
 
-
+skills = [
+    'Стремительный прыжок',
+    'Электрический выстрел',
+    'Ледяной удар',
+    'Стремительный удар',
+    'Кислотный взгляд',
+    'Тайный побег',
+    'Ледяной выстрел',
+    'Огненный заряд'
+]
 
 context = {
     'first_name':fake.first_name(),
-    'last_name' : 'Пупкин',
-    'town' : 'Пупкино',
-    'job': 'Дворник',
-    'strength': 81,
-    'agility' : 56,
-    'endurance': 65,
-    'intelligence': 45,
-    'luck': 56,
-    'skill_1' : 'Любит морковку',
-    'skill_2' : 'Супер плевок',
-    'skill_3' : 'Супер прыжок',
+    'last_name' : fake.last_name(),
+    'town' : fake.city(),
+    'job': fake.job(),
+    'strength': randint(3,18),
+    'agility' : randint(3,18),
+    'endurance': randint(3,18),
+    'intelligence': randint(3,18),
+    'luck': randint(3,18),
+    'skill_1' : choice(skills),
+    'skill_2' : choice(skills),
+    'skill_3' : choice(skills),
 }
-card_artem=render_template('meeting-python\\src\\charsheet.svg','meeting-python\\src\\artem_charsheet.svg',context)
+card_artem=render_template('meeting-python\\src\\charsheet.svg',f'meeting-python\\src\\{context["first_name"]}_{context["last_name"]}_charsheet.svg',context)
