c0d42e346a980b7a4d0275ba382a7b59e73bdc1d
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-25 20:15:41 +0300

    изменение шрифта

--- meeting-python/less5.py
+++ meeting-python/less5.py
@@ -1,8 +1,33 @@
 from file_operations import VERSION ,render_template
 from faker import Faker
-from random import randint,choice
+from random import randint,choice,sample
 fake = Faker('ru_RU')
 
+letters_mapping = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
 skills = [
     'Стремительный прыжок',
     'Электрический выстрел',
@@ -13,6 +38,7 @@ skills = [
     'Ледяной выстрел',
     'Огненный заряд'
 ]
+random_skils = sample(skills,3)
 
 context = {
     'first_name':fake.first_name(),
@@ -24,8 +50,8 @@ context = {
     'endurance': randint(3,18),
     'intelligence': randint(3,18),
     'luck': randint(3,18),
-    'skill_1' : choice(skills),
-    'skill_2' : choice(skills),
-    'skill_3' : choice(skills),
+    'skill_1' : random_skils[0],
+    'skill_2' : random_skils[1],
+    'skill_3' : random_skils[2],
 }
 card_artem=render_template('meeting-python\\src\\charsheet.svg',f'meeting-python\\src\\{context["first_name"]}_{context["last_name"]}_charsheet.svg',context)
