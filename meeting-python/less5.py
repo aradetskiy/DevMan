d77649f346a19a4a51c4e7e348aa58b09404a82e
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-25 21:44:46 +0300

    Завернул в __main__

--- meeting-python/less5.py
+++ meeting-python/less5.py
@@ -1,57 +1,72 @@
-from file_operations import VERSION ,render_template
+from file_operations import VERSION, render_template
 from faker import Faker
-from random import randint,choice,sample
+from random import randint, choice, sample
 fake = Faker('ru_RU')
 
-letters_mapping = {
-    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
-    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
-    ' ': ' '
-}
-skills = [
-    'Стремительный прыжок',
-    'Электрический выстрел',
-    'Ледяной удар',
-    'Стремительный удар',
-    'Кислотный взгляд',
-    'Тайный побег',
-    'Ледяной выстрел',
-    'Огненный заряд'
-]
-random_skils = sample(skills,3)
 
-context = {
-    'first_name':fake.first_name(),
-    'last_name' : fake.last_name(),
-    'town' : fake.city(),
-    'job': fake.job(),
-    'strength': randint(3,18),
-    'agility' : randint(3,18),
-    'endurance': randint(3,18),
-    'intelligence': randint(3,18),
-    'luck': randint(3,18),
-    'skill_1' : random_skils[0],
-    'skill_2' : random_skils[1],
-    'skill_3' : random_skils[2],
-}
-card_artem=render_template('meeting-python\\src\\charsheet.svg',f'meeting-python\\src\\{context["first_name"]}_{context["last_name"]}_charsheet.svg',context)
+def main():
+    letters_mapping = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
+    }
+    skills = [
+        'Стремительный прыжок',
+        'Электрический выстрел',
+        'Ледяной удар',
+        'Стремительный удар',
+        'Кислотный взгляд',
+        'Тайный побег',
+        'Ледяной выстрел',
+        'Огненный заряд'
+    ]
+    for i in range(10):
+        new_skills = []
+        for line in skills:
+            new_line = line
+            for key, value in letters_mapping.items():
+                new_line = new_line.replace(key, value)
+            new_skills.append(new_line)
+
+        random_skils = sample(new_skills, 3)
+
+        context = {
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+            'town': fake.city(),
+            'job': fake.job(),
+            'strength': randint(3, 18),
+            'agility': randint(3, 18),
+            'endurance': randint(3, 18),
+            'intelligence': randint(3, 18),
+            'luck': randint(3, 18),
+            'skill_1': random_skils[0],
+            'skill_2': random_skils[1],
+            'skill_3': random_skils[2],
+        }
+        render_template('meeting-python\\src\\charsheet.svg',
+                        f'meeting-python\\Repl\\{context["first_name"]}_{context["last_name"]}_charsheet.svg', context)
+
+
+if __name__ == '__main__':
+    main()
