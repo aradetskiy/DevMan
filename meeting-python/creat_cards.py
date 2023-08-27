225563761aa31c4534e6920287c14680a5280be1
a.radetskiy <a.radetskiy@rtcomm.ru>
2023-08-27 19:31:12 +0300

    ктовимся к замене буковок

--- meeting-python/creat_cards.py
+++ meeting-python/creat_cards.py
@@ -1,38 +1,64 @@
 from file_operations import render_template
 from faker import Faker
-from random import randint,sample
+from random import randint, sample
 from codecs import open
+
+
+
+def get_skills():
+    file = open('meeting-python\src\skills.txt', 'r', 'utf-8',)
+    file_skills = file.readlines()
+    file.close()
+    letters_mapping =  {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
+    }
+    skills = []
+    for line in file_skills:
+        skills.append(line)
+    npc_skills = sample(skills, 3)
+    return npc_skills
+
 
 def make_card_npc():
     fake = Faker('ru_RU')
-    
-    file = open('meeting-python\src\skills.txt','r','utf-8',)
-    file_skills = file.readlines()
-    file.close()
-    skills = []
-    for line in file_skills:
-        skills.append(line) 
-    npc_skills = sample(skills,3)
-    
+    npc_skills = get_skills()
     context = {
         'first_name': fake.first_name(),
         'last_name': fake.last_name(),
         'job': fake.job(),
         'town': fake.city(),
-        'strength' : randint(3,18),
-        'agility' : randint(3,18),
-        'endurance' : randint(3,18),
-        'intelligence' : randint(3,18),
-        'luck' : randint(3,18),
+        'strength': randint(3, 18),
+        'agility': randint(3, 18),
+        'endurance': randint(3, 18),
+        'intelligence': randint(3, 18),
+        'luck': randint(3, 18),
         'skill_1': npc_skills[0],
         'skill_2': npc_skills[1],
         'skill_3': npc_skills[2]
     }
-    
-    
 
-    
-    
     render_template('meeting-python\src\charsheet.svg',
                     f'meeting-python\Repl\\{context["first_name"]}_{context["last_name"]}.svg', context)
 
